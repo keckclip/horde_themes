10fee850853121d5e59ac79c2e93691b594e5a68
root <root@webster.inside.mauser.info>
2013-06-05 20:16:42 +0200

    pen Tasks for Matrix Theme
                    [x] Kronolith Week: Wochenenden in Farbe wie Monat
                    [x] Kronolith: Minical in Monat anpassen wie in Jahr
                    [x] Search
                    [ ] Hochladen-Knöpfe Farbe anpassen
                    [x] Lesezeichen
                    [x] Inventar
                    [x] Filemanager
                    [x] Author hinzufuegen
                    [x] Mail Scrollbar sichtbarer machen
                    [x] Suchfeld und Suchbutton Border sichtbarer machen
                    [ ] Farbschema Auswahl Skript
                        # Hole Farben
                        # grep -rRnH "#0*0" screen.css | tr ' ' '\012' | grep "#0*0" | sed 's/\(!.*\|;\|#\)//g' | sort | uniq
                            # Check mapColor.sh,
    				added 2nd one liner to sort all lines of code
    				with colors by increasing color intensity
    
                    Kronolith: Wollen wir Schnell-Termin in passender Farbe?
                    Generell: Wollen wir die Radiobuttons und Scroll-leisten in passender Farbe?

--- mapColor.sh
+++ mapColor.sh
@@ -1,2 +1,4 @@
 grep -rRnH "#0*0" screen.css | tr ' ' '\012' | grep "#0*0" | sed 's/\(!.*\|;\|#\)//g' | sort | uniq | xargs -I{} printf '%d\n' 0x{} | xargs -I{} expr {} / 16 | xargs -I{} expr {} - {} % 2 | uniq | xargs -I{} printf '%x ' {}
 
+
+grep -rRnH "#0*0" screen.css | grep "#0*0" | sed 's/\(!.*\|;\|#\|[1-A]px\|dashed\|solid\|shadow\)/   /g' | awk '{printf("%s %s %s\n",$1,$2,$3)}' | sort -k 2 
